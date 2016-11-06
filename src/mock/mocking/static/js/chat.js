590b4fd68566742edd9d9f7d13f36b43cbade70c
vincent zhou <grayy921013@gmail.com>
2016-11-05 20:43:31 -0400

    chat js access control added

--- src/mock/mocking/static/js/chat.js
+++ src/mock/mocking/static/js/chat.js
@@ -1,24 +1,26 @@
-$(function() {
+$(function () {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var interview_id = $("#interview_id").html();
     var use_id = $("#user_id").html();
+    var owner_id = $("#owner_id").html();
     var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/interview/" + interview_id);
 
-    chatsock.onmessage = function(message) {
+    if (owner_id != use_id) {
+        $("#message").prop('disabled', true);
+    }
+    chatsock.onmessage = function (message) {
         var data = JSON.parse(message.data);
         var textarea = $("#message");
 
         textarea.val(data.message);
     };
 
-    $("#chatform").on("submit", function(event) {
-        event.preventDefault();
+    $('#message').bind('input propertychange', function () {
         var message = {
             handle: use_id,
             message: $('#message').val()
         };
         chatsock.send(JSON.stringify(message));
-        return false;
     });
 });
