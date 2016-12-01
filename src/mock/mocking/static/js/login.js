ad9e23485e0053315991e77a94e1f36ba46e3281
ShuxinLin <linshuhsin@gmail.com>
2016-12-01 07:36:05 -0500

    modified ui

---
+++ src/mock/mocking/static/js/login.js
@@ -0,0 +1,5 @@
+var div = document.getElementById('login');
+
+div.style.transition="opacity 2s";
+div.style.opacity=1;
+
---
+++ src/mock/mocking/static/js/register.js
@@ -0,0 +1,5 @@
+var div = document.getElementById('register');
+
+div.style.transition="opacity 2s";
+div.style.opacity=1;
+
